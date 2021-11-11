0494a8e1a74ded53e0c9a6ebe3c678fdbb535040
YL <simon-ling@hotmail.com>
2021-11-10 20:44:51 -0500

    done

---
+++ EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemoBase : ComponentBase
+    {
+        //data binding to property
+        protected string Name { get; set; } = "Tom";
+        protected string Gender { get; set; } = "Male";
+        protected string Color { get; set; } = "background-color:white";
+    }
+}
