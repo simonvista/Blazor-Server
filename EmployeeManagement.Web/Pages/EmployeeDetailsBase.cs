34e5c9ce21c1e4c969c289bf96fca0c8cb82b70b
YL <simon-ling@hotmail.com>
2021-11-10 19:35:56 -0500

    done

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -16,15 +16,30 @@ namespace EmployeeManagement.Web.Pages
         [Inject] public IEmployeeService EmployeeService { get; set; }
         public Employee Employee { get; set; } = new Employee();
         protected string Coordinates { get; set; }  //EmployeeDetails.razor can access Coordinates
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
         protected override async Task OnInitializedAsync()  
         {
             Id = Id ?? "1";
             Employee= await EmployeeService.GetEmployee(int.Parse(Id));
         }
 
-        protected void Mouse_Mouve(MouseEventArgs e)
+        //protected void Mouse_Mouve(MouseEventArgs e)
+        //{
+        //    Coordinates = $"x: {e.ClientX}, y: {e.ClientY}";
+        //}
+        protected void Button_Click()
         {
-            Coordinates = $"x: {e.ClientX}, y: {e.ClientY}";
+            if (ButtonText=="Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                ButtonText = "Hide Footer";
+                CssClass = null;
+            }
         }
     }
 }
