bb56e011b105784327a60f2bbc2ad8c0336621fd
YL <simon-ling@hotmail.com>
2021-11-11 11:23:43 -0500

    done

--- EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -14,5 +14,11 @@ namespace EmployeeManagement.Web.Pages
         public Employee Employee { get; set; }
         [Parameter]
         public bool ShowFooter { get; set; }
+        [Parameter]
+        public EventCallback<bool> OnEmployeeSelection { get; set; }
+        protected async Task CheckBoxChanged(ChangeEventArgs e)
+        {
+            await OnEmployeeSelection.InvokeAsync((bool) e.Value);
+        }
     }
 }
--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -12,9 +12,11 @@ namespace EmployeeManagement.Web.Pages
     {
         //inject service -> no inject service through constructor
         [Inject] public IEmployeeService EmployeeService { get; set; }
-        public IEnumerable<Employee> Employees { get; set; }
+        protected IEnumerable<Employee> Employees { get; set; }
 
-        public bool ShowFooter { get; set; } = true;
+        protected bool ShowFooter { get; set; } = true;
+
+        protected int SelectedEmployeesCount { get; set; } = 0;
         //protected override Task OnInitializedAsync()
         //{
         //    LoadEmployees();
@@ -26,55 +28,16 @@ namespace EmployeeManagement.Web.Pages
             Employees= (await EmployeeService.GetEmployees()).ToList();
         }
 
-        //private void LoadEmployees()
-        //{
-        //    System.Threading.Thread.Sleep(3000);
-        //    Employee e1 = new Employee
-        //    {
-        //        EmployeeId = 1,
-        //        FirstName = "John",
-        //        LastName = "Hasting",
-        //        Email = "David@pragimteach.com",
-        //        DateOfBirth = new DateTime(1980, 10, 5),
-        //        Gender = Gender.Male,
-        //        DepartmentId = 1,
-        //        PhotoPath = "images/john.jpg"
-        //    };
-        //    Employee e2 = new Employee
-        //    {
-        //        EmployeeId = 2,
-        //        FirstName = "Sam",
-        //        LastName = "Galloway",
-        //        Email = "sam@pragimteach.com",
-        //        DateOfBirth = new DateTime(1981, 12, 22),
-        //        Gender = Gender.Male,
-        //        DepartmentId = 2,
-        //        PhotoPath = "images/sam.jpg"
-        //    };
-        //    Employee e3 = new Employee
-        //    {
-        //        EmployeeId = 3,
-        //        FirstName = "Mary",
-        //        LastName = "Smith",
-        //        Email = "mary@pragimteach.com",
-        //        DateOfBirth = new DateTime(1979, 11, 11),
-        //        Gender = Gender.Female,
-        //        DepartmentId = 1,
-        //        PhotoPath = "images/mary.jpg"
-        //    };
-        //    Employee e4 = new Employee
-        //    {
-        //        EmployeeId = 4,
-        //        FirstName = "Sara",
-        //        LastName = "Longway",
-        //        Email = "sara@pragimteach.com",
-        //        DateOfBirth = new DateTime(1982, 9, 23),
-        //        Gender = Gender.Female,
-        //        DepartmentId = 3,
-        //        PhotoPath = "images/sara.jpg"
-        //    };
-        //    Employees = new List<Employee> {e1, e2, e3, e4};
-        //}
-
+        protected void EmployeeSelectionChanged(bool isSelected)
+        {
+            if (isSelected)
+            {
+                SelectedEmployeesCount++;
+            }
+            else
+            {
+                SelectedEmployeesCount--;
+            }
+        }
     }
 }
