2e01fc45ea55291280544800d24d790dc028a948
YL <simon-ling@hotmail.com>
2021-11-11 12:34:20 -0500

    done

---
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+        //Id will be passed through URL
+        [Parameter]
+        public string Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee= await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
