6b5ebde959217822e18d39b92fcf7c14610d314f
YL <simon-ling@hotmail.com>
2021-11-10 21:11:49 -0500

    done

---
+++ EmployeeManagement.Web/Pages/DatabindingDemo1Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemo1Base :ComponentBase
+    {
+        public string Description { get; set; } = string.Empty;
+    }
+}
