5e69a7a3f15fcb105d7157059f532136c5f5bec8
YL <simon-ling@hotmail.com>
2021-11-11 19:50:26 -0500

    done

---
+++ EmployeeManagement.Web/Pages/CustomInputSelect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class CustomInputSelect<TValue> : InputSelect<TValue>
+    {
+        protected override bool TryParseValueFromString(
+            string value, out TValue result, out string validationErrorMessage)
+        {
+            if (typeof(TValue) == typeof(int))
+            {
+                if (int.TryParse(value,out var resultInt))
+                {
+                    result = (TValue) (object) resultInt;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    validationErrorMessage =
+                        $"The selected value {value} isn't a valid number";
+                    return false;
+                }
+            }
+            else
+            {
+                return base.TryParseValueFromString(value, out result, out validationErrorMessage);
+            }
+        }
+    }
+}
