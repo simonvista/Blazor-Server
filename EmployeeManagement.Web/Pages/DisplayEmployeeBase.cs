e5acfd67d2f3bf35ca19ac7638c6b0d9e1702f47
YL <simon-ling@hotmail.com>
2021-11-13 18:30:04 -0500

    done

--- EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace EmployeeManagement.Web.Pages
@@ -16,9 +17,16 @@ namespace EmployeeManagement.Web.Pages
         public bool ShowFooter { get; set; }
         [Parameter]
         public EventCallback<bool> OnEmployeeSelection { get; set; }
+        [Inject] public IEmployeeService EmployeeService { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
         protected async Task CheckBoxChanged(ChangeEventArgs e)
         {
             await OnEmployeeSelection.InvokeAsync((bool) e.Value);
+        }
+        protected async Task Delete_Click()
+        {
+            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+            NavigationManager.NavigateTo("/",true);
         }
     }
 }
